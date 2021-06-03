6911e0373afbbcd0e9251e831f9ae56c38460e12
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-06-03 22:17:13 +0900

    Add MSBuild path

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ ECHO MsBuild‚ğÀs‚µ‚Ü‚·B
 ECHO „ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª
 @ECHO ON
 
+@Set Path=C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319;%PATH%
 MSBuild src/PicSum.sln /p:OutputPath="%CD%/bin" /t:Rebuild /p:Configuration=Release
 
 pause
