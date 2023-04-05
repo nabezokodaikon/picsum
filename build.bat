09826f4a0572ff71ab15487c8cb95756d622125e
nabezokodaikon <nabezoko.daikon@gmail.com>
2023-04-05 20:24:24 +0900

    Modify MSBuild.exe path

--- build.bat
+++ build.bat
@@ -1,10 +1,4 @@
-@ECHO OFF
-ECHO „ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª
-ECHO MsBuild‚ğÀs‚µ‚Ü‚·B
-ECHO „ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª
-@ECHO ON
-
-@Set Path=C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319;%PATH%
-MSBuild src/PicSum.sln /p:OutputPath="%CD%/bin" /t:Rebuild /p:Configuration=Release
+@SET PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin";%PATH%
+MSBuild src/PicSum.sln /p:OutputPath="%CD%/bin" /t:Clean;Rebuild /p:Configuration=Release
 
 pause
