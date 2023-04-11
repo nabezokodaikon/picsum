884db28d9b81c8602b44f538f617e22e73b96dbf
nabezokodaikon <nabezoko.daikon@gmail.com>
2023-04-12 01:57:10 +0900

    Remove unnecessary sql

--- src/PicSum.Main/sql/DeletionFolderViewHistoryByFile.sql
+++
@@ -1,6 +0,0 @@
-/* TODO: t_folder_view_counter ‚ğÀ‘•‚µ‚½‚çAíœ‚·‚éB */
-DELETE FROM t_folder_view_history
- WHERE file_id = (SELECT mf.file_id
-                    FROM m_file mf
-                   WHERE mf.file_path = :file_path
-                 )
