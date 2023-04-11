aeffe400cffdffe903b30a384967de951664cf37
nabezokodaikon <nabezoko.daikon@gmail.com>
2023-04-12 00:21:12 +0900

    Add t_folder_view_counter table increment sql file

---
+++ src/PicSum.Main/sql/IncrementFolderViewCounter.sql
@@ -0,0 +1,12 @@
+UPDATE t_folder_view_counter
+    SET view_count = (SELECT tfvc.view_count + 1
+                         FROM t_folder_view_counter tfvc
+                        WHERE tfvc.file_id = (SELECT mf.file_id
+                                                  FROM m_file mf
+                                                 WHERE mf.file_path = :file_path
+                                              )
+                     )
+ WHERE file_id = (SELECT mf.file_id
+                    FROM m_file mf
+                   WHERE mf.file_path = :file_path
+                 )
