b119e0f192ae2d53f11bfccd91e010dc0905d890
nabezokodaikon <nabezoko.daikon@gmail.com>
2023-04-12 00:35:20 +0900

    Modify ReadFavoriteFolder sql

--- src/PicSum.Main/sql/ReadFavoriteFolder.sql
+++ src/PicSum.Main/sql/ReadFavoriteFolder.sql
@@ -1,10 +1,9 @@
 SELECT mf.file_path
-	FROM m_file mf
-       INNER JOIN (SELECT tfvh.file_id
-                         ,COUNT(1) AS cnt
-                     FROM t_folder_view_history tfvh
-                    GROUP BY tfvh.file_id
+  FROM m_file mf
+       INNER JOIN (SELECT tfvc.file_id
+                         ,tfvc.view_count AS cnt
+                     FROM t_folder_view_counter tfvc
+                 ORDER BY tfvc.view_count DESC
+                    LIMIT 200
                   ) t
           ON t.file_id = mf.file_id
-ORDER BY t.cnt DESC
-LIMIT 200;
