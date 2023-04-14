893fe00f6de8a75bae962b6772324b1ce483eecb
nabezokodaikon <nabezoko.daikon@gmail.com>
2023-04-14 18:36:39 +0900

    Modify build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,7 @@
 @SET PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin";%PATH%
-MSBuild src/PicSum.sln /p:OutputPath="%CD%/bin" /t:Clean;Rebuild /p:Configuration=Release;Platform="Any CPU"
+MSBuild src\PicSum.sln /p:OutputPath="%CD%\bin" /t:Clean;Rebuild /p:Configuration=Release;Platform="Any CPU"
+
+del "%CD%\bin\*.pdb"
+del "%CD%\bin\*.xml"
 
 pause
