2dbc3e0a58cb190614391b39f5a82fad10b0df2c
nabezokodaikon <nabezoko.daikon@gmail.com>
2023-04-25 18:34:55 +0900

    Clean DirectoryFileListContentsParameter

--- src/PicSum.UIComponent.Contents/ContentsParameter/DirectoryFileListContentsParameter.cs
+++ src/PicSum.UIComponent.Contents/ContentsParameter/DirectoryFileListContentsParameter.cs
@@ -1,71 +1,39 @@
-﻿using System;
+﻿using PicSum.UIComponent.Contents.FileListContents;
 using SWF.UIComponent.TabOperation;
-using PicSum.UIComponent.Contents.FileListContents;
+using System;
 
 namespace PicSum.UIComponent.Contents.ContentsParameter
 {
     /// <summary>
     /// フォルダファイルリストコンテンツパラメータ
     /// </summary>
-    public class DirectoryFileListContentsParameter : IContentsParameter
+    public class DirectoryFileListContentsParameter
+        : IContentsParameter
     {
         public const string CONTENTS_SOURCES = "Directory";
-
-        private string _directoryPath;
-        private string _selectedFilePath;
 
         public string ContentsSources { get; private set; }
         public string SourcesKey { get; private set; }
         public string Key { get; private set; }
-
-
-        public string DirectoryPath
-        {
-            get
-            {
-                return _directoryPath;
-            }
-        }
-
-        public string SelectedFilePath
-        {
-            get
-            {
-                return _selectedFilePath;
-            }
-        }
+        public string DirectoryPath { get; private set; }
+        public string SelectedFilePath { get; private set; }
 
         public DirectoryFileListContentsParameter(string directoryPath, string selectedFilePath)
         {
-            if (directoryPath == null)
-            {
-                throw new ArgumentNullException("directoryPath");
-            }
-
-            if (selectedFilePath == null)
-            {
-                throw new ArgumentNullException("selectedFilePath");
-            }
-
-            this.ContentsSources = CONTENTS_SOURCES;
+            this.ContentsSources = DirectoryFileListContentsParameter.CONTENTS_SOURCES;
             this.SourcesKey = directoryPath;
             this.Key = string.Format("{0}ListContents:{1}", this.ContentsSources, this.SourcesKey);
-            _directoryPath = directoryPath;
-            _selectedFilePath = selectedFilePath;
+            this.DirectoryPath = directoryPath ?? throw new ArgumentNullException(nameof(directoryPath));
+            this.SelectedFilePath = selectedFilePath;
         }
 
         public DirectoryFileListContentsParameter(string directoryPath)
         {
-            if (directoryPath == null)
-            {
-                throw new ArgumentNullException("directoryPath");
-            }
-
-            this.ContentsSources = CONTENTS_SOURCES;
+            this.ContentsSources = DirectoryFileListContentsParameter.CONTENTS_SOURCES;
             this.SourcesKey = directoryPath;
             this.Key = string.Format("{0}ListContents:{1}", this.ContentsSources, this.SourcesKey);
-            _directoryPath = directoryPath;
-            _selectedFilePath = string.Empty;
+            this.DirectoryPath = directoryPath ?? throw new ArgumentNullException(nameof(directoryPath));
+            this.SelectedFilePath = string.Empty;
         }
 
         public ContentsPanel CreateContents()
