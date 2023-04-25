b2f0cbdfef4858e79e891f8e994859984320a23c
nabezokodaikon <nabezoko.daikon@gmail.com>
2023-04-25 11:15:28 +0900

    Add EventArgs

---
+++ src/PicSum.UIComponent.Contents/ContentsParameter/GetImageFilesEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicSum.UIComponent.Contents.ContentsParameter
+{
+    public sealed class GetImageFilesEventArgs
+        : EventArgs
+    {
+        public IList<string> FilePathList { get; private set; }
+        public string SelectedFilePath { get; private set; }
+
+        public GetImageFilesEventArgs(IList<string> filePathList, string selectedFilePath)
+        {
+            this.FilePathList = filePathList ?? throw new ArgumentNullException(nameof(filePathList));
+            this.SelectedFilePath = selectedFilePath ?? throw new ArgumentNullException(nameof(selectedFilePath));
+        }
+    }
+}
