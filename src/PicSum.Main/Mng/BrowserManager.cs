f231ec0b22c020ab70571e58bdfbfe5788ba9a7d
nabezokodaikon <nabezoko.daikon@gmail.com>
2023-04-30 22:32:57 +0900

    Clean BrowserManager

--- src/PicSum.Main/Mng/BrowserManager.cs
+++ src/PicSum.Main/Mng/BrowserManager.cs
@@ -1,20 +1,17 @@
-﻿using System;
+﻿using PicSum.Main.UIComponent;
+using SWF.UIComponent.TabOperation;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using PicSum.Main.Properties;
-using PicSum.Main.UIComponent;
-using PicSum.Task.Entity;
-using SWF.UIComponent.TabOperation;
 
 namespace PicSum.Main.Mng
 {
     /// <summary>
     /// ブラウザ管理クラス
     /// </summary>
-    public class BrowserManager
+    public sealed class BrowserManager
     {
         #region イベント
 
@@ -27,7 +24,7 @@ namespace PicSum.Main.Mng
 
         #region インスタンス変数
 
-        private readonly List<BrowserForm> _browserList = new List<BrowserForm>();
+        private readonly List<BrowserForm> browserList = new List<BrowserForm>();
 
         #endregion
 
@@ -35,7 +32,7 @@ namespace PicSum.Main.Mng
 
         public BrowserManager()
         {
-            initializeComponent();
+
         }
 
         #endregion
@@ -48,111 +45,97 @@ namespace PicSum.Main.Mng
         /// <returns></returns>
         public BrowserForm GetActiveBrowser()
         {
-            return getActiveBrowser();
-        }
-
-        #endregion
-
-        #region イベント発生メソッド
-
-        protected virtual void OnBrowserNothing(EventArgs e)
-        {
-            if (BrowserNothing != null)
+            if (this.browserList.Count > 0)
             {
-                BrowserNothing(this, e);
-            }
-        }
-
-        #endregion
-
-        #region プライベートメソッド
-
-        private void initializeComponent()
-        {
-
-        }
-
-        private BrowserForm getActiveBrowser()
-        {
-            if (_browserList.Count > 0)
-            {
-                return _browserList.First();
+                return this.browserList.First();
             }
             else
             {
-                BrowserForm browser = createBrowser();
-                _browserList.Add(browser);
+                var browser = this.CreateBrowser();
+                this.browserList.Add(browser);
                 return browser;
             }
         }
 
         #endregion
 
-        #region ブラウザを作成メソッド
+        #region イベント発生メソッド
 
-        private BrowserForm createBrowser()
+        public void OnBrowserNothing(EventArgs e)
         {
-            BrowserForm browser = new BrowserForm();
-            initializeBrowserDelegate(browser);
+            if (this.BrowserNothing != null)
+            {
+                this.BrowserNothing(this, e);
+            }
+        }
+
+        #endregion
+
+        #region ブラウザ作成メソッド
+
+        private BrowserForm CreateBrowser()
+        {
+            var browser = new BrowserForm();
+            this.InitializeBrowserDelegate(browser);
             return browser;
         }
 
-        private BrowserForm createBrowser(Point windowLocation, Size windowSize, FormWindowState windowState)
+        private BrowserForm CreateBrowser(Point windowLocation, Size windowSize, FormWindowState windowState)
         {
-            BrowserForm browser = new BrowserForm();
+            var browser = new BrowserForm();
 
             browser.Location = windowLocation;
             browser.Size = windowSize;
             browser.WindowState = windowState;
 
-            initializeBrowserDelegate(browser);
+            this.InitializeBrowserDelegate(browser);
 
             return browser;
         }
 
-        private void initializeBrowserDelegate(BrowserForm browser)
+        private void InitializeBrowserDelegate(BrowserForm browser)
         {
-            browser.FormClosing += new System.Windows.Forms.FormClosingEventHandler(browser_FormClosing);
-            browser.Activated += new EventHandler(browser_Activated);
-            browser.TabDropouted += new EventHandler<TabDropoutedEventArgs>(browser_TabDropouted);
-            browser.NewWindowContentsOpen += new EventHandler<BrowserContentsOpenEventArgs>(browser_NewWindowContentsOpen);
+            browser.FormClosing += new FormClosingEventHandler(this.Browser_FormClosing);
+            browser.Activated += new EventHandler(this.Browser_Activated);
+            browser.TabDropouted += new EventHandler<TabDropoutedEventArgs>(this.Browser_TabDropouted);
+            browser.NewWindowContentsOpen += new EventHandler<BrowserContentsOpenEventArgs>(this.Browser_NewWindowContentsOpen);
         }
 
         #endregion
 
         #region ブラウザイベント
 
-        private void browser_FormClosing(object sender, System.Windows.Forms.FormClosingEventArgs e)
+        private void Browser_FormClosing(object sender, FormClosingEventArgs e)
         {
-            BrowserForm browser = (BrowserForm)sender;
+            var browser = (BrowserForm)sender;
 
-            _browserList.Remove(browser);
+            this.browserList.Remove(browser);
 
-            if (_browserList.Count == 0)
+            if (this.browserList.Count == 0)
             {
-                OnBrowserNothing(new EventArgs());
+                this.OnBrowserNothing(new EventArgs());
             }
         }
 
-        private void browser_Activated(object sender, EventArgs e)
+        private void Browser_Activated(object sender, EventArgs e)
         {
-            BrowserForm browser = (BrowserForm)sender;
-            _browserList.Remove(browser);
-            _browserList.Insert(0, browser);
+            var browser = (BrowserForm)sender;
+            this.browserList.Remove(browser);
+            this.browserList.Insert(0, browser);
         }
 
-        private void browser_TabDropouted(object sender, TabDropoutedEventArgs e)
+        private void Browser_TabDropouted(object sender, TabDropoutedEventArgs e)
         {
-            BrowserForm browser = createBrowser(e.WindowLocation, e.WindowSize, e.WindowState);
-            _browserList.Add(browser);
+            var browser = this.CreateBrowser(e.WindowLocation, e.WindowSize, e.WindowState);
+            this.browserList.Add(browser);
             browser.Show();
             browser.AddTab(e.Tab);
         }
 
-        private void browser_NewWindowContentsOpen(object sender, BrowserContentsOpenEventArgs e)
+        private void Browser_NewWindowContentsOpen(object sender, BrowserContentsOpenEventArgs e)
         {
-            BrowserForm browser = createBrowser();
-            _browserList.Add(browser);
+            var browser = this.CreateBrowser();
+            this.browserList.Add(browser);
             browser.Show();
             browser.AddTab(e.ContentsParameter);
         }
