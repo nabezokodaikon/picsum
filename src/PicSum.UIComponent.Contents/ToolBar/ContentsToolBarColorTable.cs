10dae418645cc004c15e569bf78fc594a370769c
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-04-02 19:22:20 +0900

    Clean

--- src/PicSum.UIComponent.Contents/ToolBar/ContentsToolBarColorTable.cs
+++ src/PicSum.UIComponent.Contents/ToolBar/ContentsToolBarColorTable.cs
@@ -1,4 +1,4 @@
-﻿using System.Drawing;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace PicSum.UIComponent.Contents.ToolBar
@@ -7,9 +7,6 @@ namespace PicSum.UIComponent.Contents.ToolBar
         : ProfessionalColorTable
     {
         private Color gradientBeginColor = Color.FromArgb(241, 244, 250);
-        private Color gradientMiddleColor = Color.FromArgb(229, 232, 242);
-        private Color gradientEndColor = Color.FromArgb(217, 220, 230);
-        private Color borderEndColor = Color.FromArgb(124, 138, 153);
 
         public override Color ToolStripGradientBegin
         {
