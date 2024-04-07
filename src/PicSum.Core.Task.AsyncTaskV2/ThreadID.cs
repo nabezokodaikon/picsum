68bc48c0eedd0d1da6367f0ee6e68b3c6e08238e
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-04-07 16:13:32 +0900

    Add ThreadID class

---
+++ src/PicSum.Core.Task.AsyncTaskV2/ThreadID.cs
@@ -0,0 +1,24 @@
+namespace PicSum.Core.Task.AsyncTaskV2
+{
+    public sealed class ThreadID
+    {
+        private static long currentID = 0;
+
+        public static ThreadID GetNew()
+        {
+            return new ThreadID(Interlocked.Increment(ref currentID));
+        }
+
+        private readonly long value;
+
+        private ThreadID(long value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return this.value.ToString();
+        }
+    }
+}
