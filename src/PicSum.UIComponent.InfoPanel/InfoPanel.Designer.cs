8432eda0e838e3e8e265d793d750f8a195a254a4
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-04-11 08:06:10 +0900

    Modify InfoPanel layout

--- src/PicSum.UIComponent.InfoPanel/InfoPanel.Designer.cs
+++ src/PicSum.UIComponent.InfoPanel/InfoPanel.Designer.cs
@@ -17,7 +17,6 @@ namespace PicSum.UIComponent.InfoPanel
         {
             this.components = new System.ComponentModel.Container();
             this.thumbnailPictureBox = new System.Windows.Forms.PictureBox();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.tagContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.tagDeleteMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tagToAllEntryMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -26,33 +25,23 @@ namespace PicSum.UIComponent.InfoPanel
             this.wideComboBox = new SWF.UIComponent.WideDropDown.WideComboBox();
             this.fileInfoLabel = new FileInfoLabel();
             ((System.ComponentModel.ISupportInitialize)this.thumbnailPictureBox).BeginInit();
-            this.panel1.SuspendLayout();
             this.tagContextMenuStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)this.tagFlowList).BeginInit();
             this.SuspendLayout();
             // 
             // thumbnailPictureBox
             // 
+            this.thumbnailPictureBox.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
             this.thumbnailPictureBox.BackColor = System.Drawing.Color.Transparent;
-            this.thumbnailPictureBox.Dock = System.Windows.Forms.DockStyle.Top;
-            this.thumbnailPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.thumbnailPictureBox.Location = new System.Drawing.Point(4, 0);
+            this.thumbnailPictureBox.Margin = new System.Windows.Forms.Padding(4, 0, 0, 0);
             this.thumbnailPictureBox.MinimumSize = new System.Drawing.Size(128, 128);
             this.thumbnailPictureBox.Name = "thumbnailPictureBox";
-            this.thumbnailPictureBox.Size = new System.Drawing.Size(512, 256);
+            this.thumbnailPictureBox.Size = new System.Drawing.Size(508, 256);
             this.thumbnailPictureBox.TabIndex = 0;
             this.thumbnailPictureBox.TabStop = false;
             this.thumbnailPictureBox.Text = "thumbnailPictureBox1";
             this.thumbnailPictureBox.Paint += this.ThumbnailPictureBox_Paint;
-            // 
-            // panel1
-            // 
-            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
-            this.panel1.BackColor = System.Drawing.Color.Transparent;
-            this.panel1.Controls.Add(this.fileInfoLabel);
-            this.panel1.Location = new System.Drawing.Point(0, 262);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(512, 125);
-            this.panel1.TabIndex = 17;
             // 
             // tagContextMenuStrip
             // 
@@ -80,11 +69,11 @@ namespace PicSum.UIComponent.InfoPanel
             // 
             this.ratingBar.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
             this.ratingBar.BackColor = System.Drawing.Color.Transparent;
-            this.ratingBar.Location = new System.Drawing.Point(0, 390);
-            this.ratingBar.Margin = new System.Windows.Forms.Padding(0);
+            this.ratingBar.Location = new System.Drawing.Point(4, 390);
+            this.ratingBar.Margin = new System.Windows.Forms.Padding(4, 0, 0, 0);
             this.ratingBar.MaximumValue = 1;
             this.ratingBar.Name = "ratingBar";
-            this.ratingBar.Size = new System.Drawing.Size(512, 56);
+            this.ratingBar.Size = new System.Drawing.Size(508, 56);
             this.ratingBar.TabIndex = 15;
             this.ratingBar.RatingButtonMouseClick += this.RatingBar_RatingButtonMouseClick;
             // 
@@ -100,10 +89,10 @@ namespace PicSum.UIComponent.InfoPanel
             this.tagFlowList.ItemTextFormatFlags = System.Drawing.StringFormatFlags.NoWrap;
             this.tagFlowList.ItemTextLineAlignment = System.Drawing.StringAlignment.Center;
             this.tagFlowList.ItemTextTrimming = System.Drawing.StringTrimming.EllipsisCharacter;
-            this.tagFlowList.Location = new System.Drawing.Point(0, 499);
-            this.tagFlowList.Margin = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.tagFlowList.Location = new System.Drawing.Point(4, 484);
+            this.tagFlowList.Margin = new System.Windows.Forms.Padding(4, 0, 0, 0);
             this.tagFlowList.Name = "tagFlowList";
-            this.tagFlowList.Size = new System.Drawing.Size(512, 216);
+            this.tagFlowList.Size = new System.Drawing.Size(508, 231);
             this.tagFlowList.TabIndex = 16;
             this.tagFlowList.TabStop = false;
             this.tagFlowList.Text = "flowList1";
@@ -116,23 +105,25 @@ namespace PicSum.UIComponent.InfoPanel
             this.wideComboBox.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
             this.wideComboBox.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 128);
             this.wideComboBox.Icon = Properties.Resources.TagIcon;
-            this.wideComboBox.Location = new System.Drawing.Point(0, 452);
-            this.wideComboBox.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.wideComboBox.Location = new System.Drawing.Point(4, 452);
+            this.wideComboBox.Margin = new System.Windows.Forms.Padding(4, 0, 0, 0);
             this.wideComboBox.Name = "wideComboBox";
-            this.wideComboBox.Size = new System.Drawing.Size(512, 38);
+            this.wideComboBox.Size = new System.Drawing.Size(508, 32);
             this.wideComboBox.TabIndex = 27;
             this.wideComboBox.DropDownOpening += this.WideComboBox_DropDownOpening;
             this.wideComboBox.AddItem += this.WideComboBox_AddItem;
             // 
             // fileInfoLabel
             // 
-            this.fileInfoLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fileInfoLabel.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            this.fileInfoLabel.BackColor = System.Drawing.Color.Transparent;
             this.fileInfoLabel.FileName = "";
             this.fileInfoLabel.FileSize = "";
             this.fileInfoLabel.FileType = "";
-            this.fileInfoLabel.Location = new System.Drawing.Point(0, 0);
+            this.fileInfoLabel.Location = new System.Drawing.Point(4, 256);
+            this.fileInfoLabel.Margin = new System.Windows.Forms.Padding(4, 0, 0, 0);
             this.fileInfoLabel.Name = "fileInfoLabel";
-            this.fileInfoLabel.Size = new System.Drawing.Size(512, 125);
+            this.fileInfoLabel.Size = new System.Drawing.Size(508, 134);
             this.fileInfoLabel.TabIndex = 0;
             this.fileInfoLabel.Text = "fileInfoLabel1";
             this.fileInfoLabel.Timestamp = "";
@@ -140,17 +131,16 @@ namespace PicSum.UIComponent.InfoPanel
             // InfoPanel
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(244)))), ((int)(((byte)(250)))));
+            this.BackColor = System.Drawing.Color.FromArgb(241, 244, 250);
             this.Controls.Add(this.wideComboBox);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.thumbnailPictureBox);
             this.Controls.Add(this.ratingBar);
             this.Controls.Add(this.tagFlowList);
+            this.Controls.Add(this.fileInfoLabel);
             this.Font = new System.Drawing.Font("Yu Gothic UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 128);
             this.Name = "InfoPanel";
             this.Size = new System.Drawing.Size(512, 715);
             ((System.ComponentModel.ISupportInitialize)this.thumbnailPictureBox).EndInit();
-            this.panel1.ResumeLayout(false);
             this.tagContextMenuStrip.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)this.tagFlowList).EndInit();
             this.ResumeLayout(false);
@@ -161,7 +151,6 @@ namespace PicSum.UIComponent.InfoPanel
         private System.Windows.Forms.PictureBox thumbnailPictureBox;
         private SWF.UIComponent.Common.RatingBar ratingBar;
         private SWF.UIComponent.FlowList.FlowList tagFlowList;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.ContextMenuStrip tagContextMenuStrip;
         private System.Windows.Forms.ToolStripMenuItem tagDeleteMenuItem;
         private System.Windows.Forms.ToolStripMenuItem tagToAllEntryMenuItem;
