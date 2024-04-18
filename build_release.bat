f10b9435dd192f35fa7fec63893ca4e5938b2eb1
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-04-18 21:54:19 +0900

    Modify Release build batch file

--- build_release.bat
+++ build_release.bat
@@ -1,8 +1,6 @@
 @SET PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin";%PATH%
 @SET OUTPUT_PATH=%CD%\bin_release
 
-RMDIR /s /q %OUTPUT_PATH%
-
 dotnet restore src\PicSum.sln
 
 MSBuild src\PicSum.sln /t:Rebuild ^
