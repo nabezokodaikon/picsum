6b7cfc0d8aa47516a10f6be0c7c665f6609ec75e
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-06-27 15:01:42 +0900

    [Clean]

--- src/SWF.Core.FileAccessor/FileUtil.cs
+++ src/SWF.Core.FileAccessor/FileUtil.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 using System.Security;
