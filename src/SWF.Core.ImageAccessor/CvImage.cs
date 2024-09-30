e09d5ee141043ad4cd63d253e578cda1e215a554
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-09-30 20:17:06 +0900

    [Mod] Image clone process

--- src/SWF.Core.ImageAccessor/CvImage.cs
+++ src/SWF.Core.ImageAccessor/CvImage.cs
@@ -10,9 +10,10 @@ namespace SWF.Core.ImageAccessor
         public static readonly CvImage EMPTY = new CvImage(ImageUtil.EMPTY_IMAGE);
 
         private bool disposed = false;
+        private readonly Mat mat;
 
         public Bitmap Bitmap { get; private set; }
-        public Mat Mat { get; private set; }
+
 
         public System.Drawing.Size Size
         {
@@ -43,7 +44,13 @@ namespace SWF.Core.ImageAccessor
             ArgumentNullException.ThrowIfNull(bitmap, nameof(bitmap));
 
             this.Bitmap = bitmap;
-            this.Mat = this.Bitmap.ToMat();
+            this.mat = this.Bitmap.ToMat();
+        }
+
+        private CvImage(Mat mat)
+        {
+            this.mat = mat;
+            this.Bitmap = this.mat.ToBitmap();
         }
 
         private void Dispose(bool disposing)
@@ -55,11 +62,6 @@ namespace SWF.Core.ImageAccessor
 
             if (disposing)
             {
-                this.Bitmap.Dispose();
-                this.Mat.Dispose();
-                this.Bitmap = null;
-                this.Mat = null;
-
                 var sw = Stopwatch.StartNew();
                 GC.Collect();
                 sw.Stop();
@@ -82,12 +84,16 @@ namespace SWF.Core.ImageAccessor
 
         public Bitmap Resize(int newWidth, int newHeight)
         {
-            return OpenCVUtil.Resize(this.Mat, newWidth, newHeight);
+            return OpenCVUtil.Resize(this.mat, newWidth, newHeight);
         }
 
         public CvImage Clone()
         {
-            return new CvImage(this.Mat.ToBitmap());
+            var sw = Stopwatch.StartNew();
+            var clone = new CvImage(this.mat);
+            sw.Stop();
+            Console.WriteLine($"Clone: {sw.ElapsedMilliseconds} ms");
+            return clone;
         }
     }
 }
--- src/SWF.Core.ImageAccessor/OpenCVUtil.cs
+++ src/SWF.Core.ImageAccessor/OpenCVUtil.cs
@@ -6,18 +6,6 @@ namespace SWF.Core.ImageAccessor
 {
     internal static class OpenCVUtil
     {
-        public static Bitmap Resize(Bitmap srcBMP, int newWidth, int newHeight)
-        {
-            ArgumentNullException.ThrowIfNull(srcBMP, nameof(srcBMP));
-
-            using (var srcMat = srcBMP.ToMat())
-            using (var destMat = new Mat())
-            {
-                Cv2.Resize(srcMat, destMat, new OpenCvSharp.Size(newWidth, newHeight), 0, 0, InterpolationFlags.Area);
-                return destMat.ToBitmap();
-            }
-        }
-
         public static Bitmap Resize(Mat srcMat, int newWidth, int newHeight)
         {
             ArgumentNullException.ThrowIfNull(srcMat, nameof(srcMat));
@@ -27,16 +15,6 @@ namespace SWF.Core.ImageAccessor
                 Cv2.Resize(srcMat, destMat, new OpenCvSharp.Size(newWidth, newHeight), 0, 0, InterpolationFlags.Area);
                 var destBMP = destMat.ToBitmap();
                 return destBMP;
-            }
-        }
-
-        public static Bitmap Clone(Bitmap src)
-        {
-            ArgumentNullException.ThrowIfNull(src, nameof(src));
-
-            using (var m = src.ToMat())
-            {
-                return m.ToBitmap();
             }
         }
     }
