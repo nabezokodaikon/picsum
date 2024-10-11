61e09176e0c311d6148e7f43b9aa0867e6b326a9
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-10-11 21:55:35 +0900

    [Mod] Instance on EmptyResult

--- src/PicSum.Core.Job.AsyncJob/EmptyResult.cs
+++ src/PicSum.Core.Job.AsyncJob/EmptyResult.cs
@@ -3,6 +3,8 @@ namespace PicSum.Core.Job.AsyncJob
     public sealed class EmptyResult
         : IJobResult
     {
+        public static readonly EmptyResult Value = new();
+
         private EmptyResult()
         {
 
