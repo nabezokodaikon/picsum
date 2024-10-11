b46316296dfb7e0f5a6b9aa6c6c3e1613eb16649
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-10-12 04:41:36 +0900

    [Add] Cache set function on ImageFileInfoCacheUtil

--- src/SWF.Core.ImageAccessor/ImageFileInfoCacheUtil.cs
+++ src/SWF.Core.ImageAccessor/ImageFileInfoCacheUtil.cs
@@ -125,5 +125,59 @@ namespace SWF.Core.ImageAccessor
                 //Console.WriteLine($"[{Thread.CurrentThread.Name}] ImageFileInfoCacheUtil.Get: {sw.ElapsedMilliseconds} ms");
             }
         }
+
+        public static void Set(string filePath, Size size)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(filePath, nameof(filePath));
+
+            var sw = Stopwatch.StartNew();
+
+            var timestamp = FileUtil.GetUpdateDate(filePath);
+
+            CACHE_LOCK.EnterUpgradeableReadLock();
+            try
+            {
+                if (CACHE_DICTIONARY.TryGetValue(filePath, out var cache))
+                {
+                    if (timestamp == cache.Timestamp)
+                    {
+                        return;
+                    }
+                }
+
+                CACHE_LOCK.EnterWriteLock();
+                try
+                {
+                    if (cache != null)
+                    {
+                        CACHE_LIST.Remove(cache);
+                        CACHE_DICTIONARY.Remove(cache.FilePath);
+                    }
+
+                    if (CACHE_LIST.Count > CACHE_CAPACITY)
+                    {
+                        var removeCache = CACHE_LIST[0];
+                        CACHE_LIST.Remove(removeCache);
+                        CACHE_DICTIONARY.Remove(removeCache.FilePath);
+                    }
+
+                    var newCache = new ImageFileInfoCache(
+                        filePath, size, timestamp);
+                    CACHE_DICTIONARY.Add(newCache.FilePath, newCache);
+                    CACHE_LIST.Add(newCache);
+                }
+                finally
+                {
+                    CACHE_LOCK.ExitWriteLock();
+                }
+            }
+            finally
+            {
+                CACHE_LOCK.ExitUpgradeableReadLock();
+
+                sw.Stop();
+                //Console.WriteLine($"[{Thread.CurrentThread.Name}] ImageFileInfoCacheUtil.Create: {sw.ElapsedMilliseconds} ms");
+            }
+        }
     }
 }
