ba348c68dca246877d90c64eb8b549b950335827
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-10-26 11:21:28 +0900

    [Clean] Build pratform

--- build_release.bat
+++ build_release.bat
@@ -7,13 +7,5 @@ dotnet restore src\PicSum.sln
 
 MSBuild src\PicSum.sln /t:Rebuild ^
   /p:Configuration=Release ^
-  /p:DebugType=None ^
   /p:OutputPath="%OUTPUT_PATH%" ^
   /p:Platform="x64"
-
-DEL "%OUTPUT_PATH%\NLog.debug.config"
-DEL "%OUTPUT_PATH%\*.pdb"
-
-RENAME "%OUTPUT_PATH%\picsum.runtimeconfig.json" "temp"
-DEL "%OUTPUT_PATH%\*.runtimeconfig.json"
-RENAME "%OUTPUT_PATH%\temp" "picsum.runtimeconfig.json"
