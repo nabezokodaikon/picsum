620d348958d14e73134f170841cc928e61f0dce2
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-11-01 08:00:10 +0900

    [Add] Publish build batch

---
+++ build_publish.bat
@@ -0,0 +1,12 @@
+@SET PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin";%PATH%
+@SET OUTPUT_PATH=%CD%\bin_
+
+MSBuild src\PicSum.sln /t:Publish ^
+  /p:Configuration=Release ^
+  /p:OutputPath="%OUTPUT_PATH%" ^
+  /p:Platform="x64"
+
+RD /S /Q "%OUTPUT_PATH%"
+DEL /Q "%OUTPUT_PATH%publish\*.dll"
+DEL /Q "%OUTPUT_PATH%publish\*.json"
+RD /S /Q "%OUTPUT_PATH%publish\runtimes"
--- build_release.bat
+++ build_release.bat
@@ -1,13 +1,12 @@
 @SET PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin";%PATH%
 @SET OUTPUT_PATH=%CD%\bin_release
 
+DEL /Q "%OUTPUT_PATH%\*.*"
+RD /S /Q "%OUTPUT_PATH%\runtimes"
+
 dotnet restore src\PicSum.sln
 
-MSBuild src\PicSum.sln /t:Publish ^
+MSBuild src\PicSum.sln /t:Rebuild ^
   /p:Configuration=Release ^
   /p:OutputPath="%OUTPUT_PATH%" ^
   /p:Platform="x64"
-
-DEL /Q "%OUTPUT_PATH%publish\*.dll"
-DEL /Q "%OUTPUT_PATH%publish\*.json"
-RD /S /Q "%OUTPUT_PATH%publish\runtimes"
