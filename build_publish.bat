2ab0ca2d4cd5a7a7a294653432f725a5ab54a85a
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-11-01 13:39:47 +0900

    [Mod] Use dotnet command on publish batch

--- build_publish.bat
+++ build_publish.bat
@@ -1,11 +1,10 @@
 @SET PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin";%PATH%
-@SET OUTPUT_PATH=%CD%\bin_
+@SET OUTPUT_PATH=%CD%\bin_publish
 
 dotnet restore src\PicSum.Main\PicSum.Main.csproj
 
-MSBuild src\PicSum.Main\PicSum.Main.csproj /t:Publish ^
-  /p:Configuration=Release ^
-  /p:OutputPath="%OUTPUT_PATH%" ^
-  /p:Platform="x64"
-
-RD /S /Q "%OUTPUT_PATH%"
+dotnet publish src\PicSum.Main\PicSum.Main.csproj ^
+  -c Release ^
+  -o "%OUTPUT_PATH%" ^
+  -p:Platform="x64" ^
+  --no-self-contained
