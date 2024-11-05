f8d988ef6401c4c0f8eae2420970172a52457132
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-11-05 15:52:16 +0900

    [Add] Test project

---
+++ src/PicSum.DatabaseAccessorTest/FileInfoDBTest.cs
@@ -0,0 +1,53 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using PicSum.DatabaseAccessor.Connection;
+using PicSum.DatabaseAccessor.Sql;
+using SWF.Core.Base;
+
+namespace PicSum.DatabaseAccessorTest
+{
+    public class FileInfoDBTest
+        : IDisposable
+    {
+        public FileInfoDBTest()
+        {
+            Directory.CreateDirectory(AppConstants.DATABASE_DIRECTORY);
+            if (!Directory.Exists(AppConstants.DATABASE_DIRECTORY))
+            {
+                Directory.CreateDirectory(AppConstants.DATABASE_DIRECTORY);
+            }
+
+            Instance<IFileInfoDB>.Initialize(
+                new FileInfoDB(AppConstants.FILE_INFO_DATABASE_FILE));
+        }
+
+        public void Dispose()
+        {
+            Instance<IFileInfoDB>.Value.Dispose();
+
+            if (Directory.Exists(AppConstants.DATABASE_DIRECTORY))
+            {
+                Directory.Delete(AppConstants.DATABASE_DIRECTORY, true);
+            }
+        }
+
+        [Fact]
+        public void FileCreationSqlTest()
+        {
+            using (var tran = Instance<IFileInfoDB>.Value.BeginTransaction())
+            {
+                var creationSql = new FileCreationSql("test");
+                Instance<IFileInfoDB>.Value.Update(creationSql);
+
+                var readSql = new AllFilesReadSql();
+                var dtoList = Instance<IFileInfoDB>.Value.ReadList(readSql);
+
+                Assert.Single(dtoList);
+
+                Assert.Equal(1, dtoList[0].FileID);
+                Assert.Equal("test", dtoList[0].FilePath);
+
+                tran.Commit();
+            }
+        }
+    }
+}
