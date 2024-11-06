90ac57c503485128724b9c8f5e105fc9730a314d
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-11-06 21:54:39 +0900

    [Fix] OverflowDrawItem is not drawn

--- src/PicSum.UIComponent.AddressBar/OverflowDrawItem.cs
+++ src/PicSum.UIComponent.AddressBar/OverflowDrawItem.cs
@@ -70,11 +70,13 @@ namespace PicSum.UIComponent.AddressBar
                 var height = Math.Min(MAXIMUM_SHOW_ITEM_COUNT * base.DropDownList.ItemHeight,
                                       base.Items.Count * base.DropDownList.ItemHeight);
 
+                base.DropDownList.BeginUpdate();
                 base.DropDownList.Size = new Size(width + base.DropDownList.ItemHeight, height);
                 base.DropDownList.ClearSelectedItems();
                 base.DropDownList.ItemCount = 0;
                 base.DropDownList.ItemCount = base.Items.Count;
                 base.DropDownList.Show(base.AddressBar, base.Left, base.Bottom);
+                base.DropDownList.EndUpdate();
             }
         }
 
