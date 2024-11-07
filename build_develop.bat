a0486237319f0def8b313503b593a0c0226ef870
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-11-07 13:28:12 +0900

    [Add] Develop build batch file

---
+++ build_develop.bat
@@ -0,0 +1,15 @@
+@SET OUTPUT_PATH=%CD%\bin_develop
+
+DEL /Q "%OUTPUT_PATH%\*.*"
+RD /S /Q "%OUTPUT_PATH%\runtimes"
+
+dotnet restore src\PicSum.Main\PicSum.Main.csproj 
+
+dotnet clean src\PicSum.Main\PicSum.Main.csproj ^
+  -c Develop ^
+  -p:Platform="x64"
+
+dotnet build src\PicSum.Main\PicSum.Main.csproj ^
+  -c Develop ^
+  -o "%OUTPUT_PATH%" ^
+  -p:Platform="x64"
