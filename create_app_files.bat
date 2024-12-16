d719323ec5f297f2391ec1c7a2bf88e5b7efeb6b
nabezokodaikon <nabezoko.daikon@gmail.com>
2024-12-16 19:47:13 +0900

    [Add] create_app_files.bat

---
+++ create_app_files.bat
@@ -0,0 +1,7 @@
+@SET PICSUM_FILES=%USERPROFILE%\AppData\Local\Packages\NabezokoDaikon.49446294FC8C5_3qnydrnvfr8aa\LocalState\picsum.files
+
+RMDIR /S /Q %PICSUM_FILES%
+MKDIR %PICSUM_FILES%
+
+MKLINK /D %PICSUM_FILES%\db %CD%\bin_publish\db
+MKLINK /D %PICSUM_FILES%\config %CD%\bin_publish\config
