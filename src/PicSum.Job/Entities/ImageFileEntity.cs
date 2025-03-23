9c1c4fd520c0e0dc4167b7b594341c7ce7a38a52
nabezokodaikon <nabezoko.daikon@gmail.com>
2025-03-23 20:13:08 +0900

    [Mod] ImageFileEntity class to struct

--- src/PicSum.Job/Entities/ImageFileEntity.cs
+++ src/PicSum.Job/Entities/ImageFileEntity.cs
@@ -5,11 +5,22 @@ namespace PicSum.Job.Entities
     /// <summary>
     /// 画像ファイルエンティティ
     /// </summary>
-    public sealed class ImageFileEntity
+    public struct ImageFileEntity
+        : IEquatable<ImageFileEntity>
     {
         public string? FilePath { get; internal set; }
         public CvImage? Image { get; internal set; }
         public bool IsEmpty { get; internal set; }
         public bool IsError { get; internal set; }
+
+        public readonly bool Equals(ImageFileEntity other)
+        {
+            if (this.FilePath != other.FilePath) { return false; }
+            if (this.Image != other.Image) { return false; }
+            if (this.IsEmpty != other.IsEmpty) { return false; }
+            if (this.IsError != other.IsError) { return false; }
+
+            return true;
+        }
     }
 }
--- src/PicSum.Job/Results/ImageFileReadResult.cs
+++ src/PicSum.Job/Results/ImageFileReadResult.cs
@@ -6,13 +6,13 @@ namespace PicSum.Job.Results
     public struct ImageFileReadResult
         : IJobResult, IEquatable<ImageFileReadResult>
     {
-        public ImageFileEntity? Image { get; internal set; }
+        public ImageFileEntity Image { get; internal set; }
         public bool IsMain { get; internal set; }
         public bool HasSub { get; internal set; }
 
         public bool Equals(ImageFileReadResult other)
         {
-            if (this.Image != other.Image) { return false; }
+            if (!this.Image.Equals(other.Image)) { return false; }
             if (this.IsMain != other.IsMain) { return false; }
             if (this.HasSub != other.HasSub) { return false; }
 
