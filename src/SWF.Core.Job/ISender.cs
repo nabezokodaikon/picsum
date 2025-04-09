b2584042bc12f405faadea4a71d83470a08b2e08
nabezokodaikon <nabezoko.daikon@gmail.com>
2025-04-09 10:17:48 +0900

    [Del] Unuse property in ISender

--- src/SWF.Core.Job/ISender.cs
+++ src/SWF.Core.Job/ISender.cs
@@ -2,10 +2,7 @@ namespace SWF.Core.Job
 {
     public interface ISender
     {
-        bool InvokeRequired { get; }
         bool IsHandleCreated { get; }
         bool IsDisposed { get; }
-        IAsyncResult BeginInvoke(Delegate method);
-        void Invoke(Action method);
     }
 }
