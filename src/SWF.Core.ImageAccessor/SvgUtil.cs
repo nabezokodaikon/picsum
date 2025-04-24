51be204a97dd3b5f307b4b2405516dc36c78b885
nabezokodaikon <nabezoko.daikon@gmail.com>
2025-04-25 06:38:55 +0900

    [Mod] Read method of svg file

--- src/SWF.Core.ImageAccessor/SvgUtil.cs
+++ src/SWF.Core.ImageAccessor/SvgUtil.cs
@@ -1,7 +1,10 @@
 using Svg;
+using SWF.Core.Base;
+using System.Runtime.Versioning;
 
 namespace SWF.Core.ImageAccessor
 {
+    [SupportedOSPlatform("windows10.0.17763.0")]
     internal static class SvgUtil
     {
         public static Size GetImageSize(Stream stream)
@@ -17,7 +20,18 @@ namespace SWF.Core.ImageAccessor
             ArgumentNullException.ThrowIfNull(stream, nameof(stream));
 
             var doc = SvgDocument.Open<SvgDocument>(stream);
-            return doc.Draw();
+
+            var (width, height) = ((int)doc.Width.Value, (int)doc.Height.Value);
+            var bitmap = new Bitmap(width, height);
+
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                using (TimeMeasuring.Run(false, "SVGUtil.ReadImageFile Draw"))
+                {
+                    doc.Draw(g, new SizeF(width, height));
+                    return bitmap;
+                }
+            }
         }
     }
 }
