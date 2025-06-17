943bd6536705d315364f8f7f05df14698cbbae0f
nabezokodaikon <nabezoko.daikon@gmail.com>
2025-06-17 22:52:11 +0900

    [Mod] Using Parallel.ForEach in ThumbnailsGetJob

--- src/PicSum.Job/Jobs/ThumbnailsGetJob.cs
+++ src/PicSum.Job/Jobs/ThumbnailsGetJob.cs
@@ -1,9 +1,15 @@
 using PicSum.Job.Common;
+using PicSum.Job.Entities;
 using PicSum.Job.Parameters;
 using PicSum.Job.Results;
 using SWF.Core.Base;
+using SWF.Core.ConsoleAccessor;
+using SWF.Core.FileAccessor;
+using SWF.Core.ImageAccessor;
 using SWF.Core.Job;
+using System.Drawing;
 using System.Runtime.Versioning;
+using ZLinq;
 
 namespace PicSum.Job.Jobs
 {
@@ -14,6 +20,11 @@ namespace PicSum.Job.Jobs
     public sealed class ThumbnailsGetJob
         : AbstractTwoWayJob<ThumbnailsGetParameter, ThumbnailImageResult>
     {
+        private static readonly ParallelOptions _parallelOptions = new ParallelOptions
+        {
+            MaxDegreeOfParallelism = 1
+        };
+
         protected override Task Execute(ThumbnailsGetParameter param)
         {
             if (param.FilePathList == null)
@@ -21,7 +32,70 @@ namespace PicSum.Job.Jobs
                 throw new ArgumentException("ファイルパスリストがNULLです。", nameof(param));
             }
 
-            Instance<IThumbnailCacheTasks>.Value.DoCache(param, this.Callback);
+            //Instance<IThumbnailCacheTasks>.Value.DoCache(param, this.Callback);
+
+            var logger = Log.GetLogger();
+
+            var filePathList = param.FilePathList
+                .AsValueEnumerable()
+                .Skip(param.FirstIndex)
+                .Take(param.LastIndex - param.FirstIndex + 1)
+                .ToArray();
+
+            Parallel.ForEach(
+                filePathList,
+                //_parallelOptions,
+                filePath =>
+                {
+                    try
+                    {
+                        this.CheckCancel();
+
+                        var bf = Instance<IThumbnailCacher>.Value.GetOrCreateCache(
+                            filePath, param.ThumbnailWidth, param.ThumbnailHeight);
+                        if (param.IsExecuteCallback
+                            && bf != ThumbnailCacheEntity.EMPTY
+                            && bf.ThumbnailBuffer != null)
+                        {
+                            Instance<IImageFileSizeCacher>.Value.Set(
+                                bf.FilePath,
+                                new Size(bf.SourceWidth, bf.SourceHeight),
+                                bf.FileUpdatedate);
+
+                            var img = new ThumbnailImageResult
+                            {
+                                FilePath = bf.FilePath,
+                                ThumbnailImage = new CvImage(
+                                    bf.FilePath,
+                                    ThumbnailUtil.ToImage(bf.ThumbnailBuffer)),
+                                ThumbnailWidth = bf.ThumbnailWidth,
+                                ThumbnailHeight = bf.ThumbnailHeight,
+                                SourceWidth = bf.SourceWidth,
+                                SourceHeight = bf.SourceHeight,
+                                FileUpdatedate = bf.FileUpdatedate
+                            };
+
+                            this.Callback(img);
+                        }
+                    }
+                    catch (JobCancelException)
+                    {
+                        return;
+                    }
+                    catch (FileUtilException ex)
+                    {
+                        logger.Error(ex);
+                    }
+                    catch (ImageUtilException ex)
+                    {
+                        logger.Error(ex);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Error(ex, $"サムネイル取得ジョブで補足されない例外が発生しました。");
+                    }
+                }
+            );
 
             return Task.CompletedTask;
         }
