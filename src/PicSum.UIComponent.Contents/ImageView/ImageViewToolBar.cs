6874b9e5421b8914e3d628f57d4c576f5860ea45
nabezokodaikon <nabezoko.daikon@gmail.com>
2025-07-18 15:52:22 +0900

    [Fix] Menu font  scale size

--- src/PicSum.UIComponent.Contents/ImageView/ImageViewToolBar.cs
+++ src/PicSum.UIComponent.Contents/ImageView/ImageViewToolBar.cs
@@ -270,6 +270,10 @@ namespace PicSum.UIComponent.Contents.ImageView
             this.singlePreviewButton.Font = buttonFont;
             this.singleNextButton.Font = buttonFont;
             this.doubleNextButton.Font = buttonFont;
+
+            var menuFont = Fonts.GetRegularFont(Fonts.Size.Medium, scale);
+            this.viewMenu.Font = menuFont;
+            this.sizeMenu.Font = menuFont;
         }
 
         protected override void Dispose(bool disposing)
