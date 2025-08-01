2ad4453ee184768eafc4b631f994c137571fe7e7
nabezokodaikon <nabezoko.daikon@gmail.com>
2025-08-01 11:29:54 +0900

    [Mod] Get shooting date and time from Xmp

--- src/SWF.Core.ImageAccessor/MetadataExtractorUtil.cs
+++ src/SWF.Core.ImageAccessor/MetadataExtractorUtil.cs
@@ -1,5 +1,6 @@
 using MetadataExtractor;
 using MetadataExtractor.Formats.Exif;
+using MetadataExtractor.Formats.Xmp;
 using SWF.Core.Base;
 using SWF.Core.FileAccessor;
 using System.Runtime.Versioning;
@@ -20,22 +21,62 @@ namespace SWF.Core.ImageAccessor
                 using (var fs = new FileStream(filePath,
                     FileMode.Open, FileAccess.Read, FileShare.Read, FILE_READ_BUFFER_SIZE, FileOptions.SequentialScan))
                 {
-                    var directories = ImageMetadataReader.ReadMetadata(filePath);
-                    var exifSubIfdDirectory = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault();
-                    if (exifSubIfdDirectory == null)
+                    var directories = ImageMetadataReader.ReadMetadata(fs);
+
+                    var date = GetPhotographDateByExifSubIfdDirectory(directories);
+                    if (date != FileUtil.EMPTY_DATETIME)
                     {
-                        return FileUtil.EMPTY_DATETIME;
+                        return date;
                     }
 
-                    if (!exifSubIfdDirectory.TryGetDateTime(ExifSubIfdDirectory.TagDateTimeOriginal, out var dateTimeOriginal))
+                    date = GetPhotographDateByXmpDirectory(directories);
+                    if (date != FileUtil.EMPTY_DATETIME)
                     {
-                        return FileUtil.EMPTY_DATETIME;
+                        return date;
                     }
 
-                    return dateTimeOriginal;
+                    return FileUtil.EMPTY_DATETIME;
                 }
             }
+        }
 
+        private static DateTime GetPhotographDateByExifSubIfdDirectory(
+            IReadOnlyList<MetadataExtractor.Directory> directories)
+        {
+            var dir = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault();
+            if (dir == null)
+            {
+                return FileUtil.EMPTY_DATETIME;
+            }
+
+            if (!dir.TryGetDateTime(ExifSubIfdDirectory.TagDateTimeOriginal, out DateTime dateTimeOriginal))
+            {
+                return FileUtil.EMPTY_DATETIME;
+            }
+
+            return dateTimeOriginal;
+        }
+
+        private static DateTime GetPhotographDateByXmpDirectory(
+            IReadOnlyList<MetadataExtractor.Directory> directories)
+        {
+            var dir = directories.OfType<XmpDirectory>().FirstOrDefault();
+            if (dir == null)
+            {
+                return FileUtil.EMPTY_DATETIME;
+            }
+
+            if (!dir.GetXmpProperties().TryGetValue("xmp:CreateDate", out string? createDateString))
+            {
+                return FileUtil.EMPTY_DATETIME;
+            }
+
+            if (!DateTimeOffset.TryParse(createDateString, out DateTimeOffset createDateOffset))
+            {
+                return FileUtil.EMPTY_DATETIME;
+            }
+
+            return createDateOffset.DateTime;
         }
     }
 }
