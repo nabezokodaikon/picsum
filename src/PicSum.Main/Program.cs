59db6d2edcff8df446416a3a105f824cbe1c4d1e
nabezokodaikon <nabezoko.daikon@gmail.com>
2025-08-16 12:15:24 +0900

    [Mod] Program class to static class

--- src/PicSum.Main/Program.cs
+++ src/PicSum.Main/Program.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace PicSum.Main
 {
-    internal sealed class Program
+    internal static class Program
     {
         /// <summary>
         /// アプリケーションのメイン エントリ ポイントです。
