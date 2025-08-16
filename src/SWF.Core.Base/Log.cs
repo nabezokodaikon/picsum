5297e0622a73bbc7950081717e0082b2c465926e
nabezokodaikon <nabezoko.daikon@gmail.com>
2025-08-16 13:27:39 +0900

    [Mod] Logger initialize process

--- src/SWF.Core.Base/Log.cs
+++ src/SWF.Core.Base/Log.cs
@@ -1,43 +1,53 @@
 using NLog;
-using NLog.Config;
 using NLog.Targets;
 
 namespace SWF.Core.Base
 {
     public static class Log
     {
-        public const string NLOG_PROPERTY = "task";
+        private static Logger? _logger = null;
 
         public static void Initialize(string logDirectory)
         {
             ArgumentNullException.ThrowIfNullOrEmpty(logDirectory, nameof(logDirectory));
 
+            if (_logger != null)
+            {
+                throw new InvalidOperationException("ロガーは初期化されています。");
+            }
+
             using (TimeMeasuring.Run(true, "Log.Initialize"))
             {
-                var config = new LoggingConfiguration();
-
-#pragma warning disable CA2000
-                var logfile = new FileTarget("logfile")
+#pragma warning disable CA2000 // スコープを失う前にオブジェクトを破棄
+                _logger = new LogFactory().Setup().LoadConfiguration(builder =>
                 {
-                    FileName = Path.Combine(logDirectory, "app.log"),
-                    Layout = "${date:format=yyyy-MM-dd HH\\:mm\\:ss.fff} | ${level:padding=-5} | ${threadid:padding=4} | ${threadname:padding=-14} | ${message:withexception=true}",
-                    ArchiveFileName = Path.Combine(logDirectory, "archive", "app_{#}.log"),
-                    ArchiveAboveSize = 10 * 1024 * 1024,
-                    MaxArchiveFiles = 30,
-                };
-#pragma warning restore CA2000
+                    builder.ForLogger().FilterMinLevel(
 #if DEBUG
-                config.AddRule(LogLevel.Trace, LogLevel.Fatal, logfile);
+                        LogLevel.Trace
 #else
-                config.AddRule(LogLevel.Info, LogLevel.Fatal, logfile);
+                        LogLevel.Info
 #endif
-                LogManager.Configuration = config;
+                    ).WriteTo(new FileTarget("logfile")
+                    {
+                        FileName = Path.Combine(logDirectory, "app.log"),
+                        Layout = "${date:format=yyyy-MM-dd HH\\:mm\\:ss.fff} | ${level:padding=-5} | ${threadid:padding=4} | ${threadname:padding=-14} | ${message:withexception=true}",
+                        ArchiveFileName = Path.Combine(logDirectory, "archive", "app_{#}.log"),
+                        ArchiveAboveSize = 10 * 1024 * 1024,
+                        MaxArchiveFiles = 30,
+                    });
+                }).GetLogger("mylogger");
+#pragma warning restore CA2000
             }
         }
 
         public static Logger GetLogger()
         {
-            return LogManager.GetCurrentClassLogger();
+            if (_logger == null)
+            {
+                throw new InvalidOperationException("ロガーが初期化されていません。");
+            }
+
+            return _logger;
         }
     }
 }
