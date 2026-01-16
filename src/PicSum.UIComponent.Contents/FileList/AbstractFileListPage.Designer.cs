92b95e72220db0144c9bc8283fb178b4d6ed52f1
nabezokodaikon <nabezoko.daikon@gmail.com>
2026-01-16 19:09:16 +0900

    [Mod] Smooth scrolling parameters

--- src/PicSum.UIComponent.Contents/FileList/AbstractFileListPage.Designer.cs
+++ src/PicSum.UIComponent.Contents/FileList/AbstractFileListPage.Designer.cs
@@ -31,7 +31,7 @@ namespace PicSum.UIComponent.Contents.FileList
             this.flowList.ItemTextAlignment = System.Drawing.StringAlignment.Center;
             this.flowList.ItemTextLineAlignment = System.Drawing.StringAlignment.Center;
             this.flowList.ItemTextTrimming = System.Drawing.StringTrimming.EllipsisCharacter;
-            this.flowList.MouseWheelRate = 0.8f;
+            this.flowList.MouseWheelRate = 1.6f;
             this.flowList.ItemDelete += new System.EventHandler(this.FlowList_ItemDelete);
             this.flowList.DrawItem += new System.EventHandler<SWF.UIComponent.FlowList.DrawItemEventArgs>(this.FlowList_Drawitem);
             this.flowList.DrawItemChanged += new System.EventHandler<SWF.UIComponent.FlowList.DrawItemChangedEventArgs>(this.FlowList_DrawItemChanged);
