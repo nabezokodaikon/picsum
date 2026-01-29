d1e6896717b9eb54f525da7289e9a0baf12f8c9c
nabezokodaikon <nabezoko.daikon@gmail.com>
2026-01-29 18:52:15 +0900

    [Mod] Speed ​​up image loading with SkiaSharp

--- src/SWF.Core.ImageAccessor/SkiaBitmapUtil.cs
+++ src/SWF.Core.ImageAccessor/SkiaBitmapUtil.cs
@@ -1,6 +1,7 @@
 ﻿using SkiaSharp;
 using SkiaSharp.Views.Desktop;
 using SWF.Core.Base;
+using System.Drawing.Imaging;
 
 namespace SWF.Core.ImageAccessor
 {
@@ -8,11 +9,55 @@ namespace SWF.Core.ImageAccessor
     {
         private const int WEBP_QUALITY = 80;
 
-        public static Bitmap ToBitmap(SKBitmap src)
+        public static Bitmap ReadImageFile(Stream stream)
+        {
+            ArgumentNullException.ThrowIfNull(stream, nameof(stream));
+
+            using (Measuring.Time(false, "SkiaBitmapUtil.ReadImageFile"))
+            {
+                ArgumentNullException.ThrowIfNull(stream, nameof(stream));
+                if (stream.CanSeek)
+                {
+                    stream.Seek(0, SeekOrigin.Begin);
+                }
+
+                using var managedStream = new SKManagedStream(stream);
+                using var codec = SKCodec.Create(managedStream);
+
+                var info = codec.Info;
+                var bitmap = new Bitmap(info.Width, info.Height, PixelFormat.Format32bppPArgb);
+
+                var data = bitmap.LockBits(
+                    new Rectangle(0, 0, info.Width, info.Height),
+                    ImageLockMode.WriteOnly,
+                    PixelFormat.Format32bppPArgb);
+
+                var options = new SKCodecOptions(SKZeroInitialized.No);
+                var decodeInfo = new SKImageInfo(info.Width, info.Height, SKColorType.Bgra8888, SKAlphaType.Premul);
+
+                var result = codec.GetPixels(decodeInfo, data.Scan0);
+                if (result != SKCodecResult.Success)
+                {
+                    bitmap.UnlockBits(data);
+                    bitmap.Dispose();
+                    throw new InvalidOperationException($"Decode failed: {result}");
+                }
+
+                bitmap.UnlockBits(data);
+                return bitmap;
+            }
+        }
+
+        public static Bitmap ToBitmap(SKBitmap skBitmap)
         {
             using (Measuring.Time(false, "SkiaBitmapUtil.ToBitmap"))
             {
-                return src.ToBitmap();
+                if (skBitmap == null)
+                {
+                    throw new InvalidOperationException("Failed to decode image.");
+                }
+
+                return skBitmap.ToBitmap();
             }
         }
 
@@ -23,29 +68,6 @@ namespace SWF.Core.ImageAccessor
                 ArgumentNullException.ThrowIfNull(src, nameof(src));
 
                 return src.ToSKBitmap();
-            }
-        }
-
-        public static Bitmap ReadImageFile(Stream stream)
-        {
-            ArgumentNullException.ThrowIfNull(stream, nameof(stream));
-
-            using (Measuring.Time(false, "SkiaBitmapUtil.ReadImageFile"))
-            {
-                if (stream.CanSeek)
-                {
-                    stream.Seek(0, SeekOrigin.Begin);
-                }
-
-                using (var skBitmap = SKBitmap.Decode(stream))
-                {
-                    if (skBitmap == null)
-                    {
-                        throw new InvalidOperationException("Failed to decode image from stream.");
-                    }
-
-                    return ToBitmap(skBitmap);
-                }
             }
         }
 
